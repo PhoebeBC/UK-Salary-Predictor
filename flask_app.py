037542cd2f7243631b3e57ab24a38e0222244d52
PhoebeBC <phoebe.badcock@mail.com>
2024-05-23 16:10:21 +0100

    created draft for front end

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,28 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+#tells flask where to go
 @app.route("/")
-def index():
-    return render_template("templates/main_page.html")
+def home():
+    return render_template("basic.html", salary_band="All", interpolated_prediction="25K")
 
+
+# This function gets the name param from the user and allows us to store and use the value
+# In this case when you add /"some_text" onto the url it will show the message bellow using the value
+# we gave in the url
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}"
+
+# this redirects a user to the page defined with the function home
+# if function has params we need to give them just like when we call a function
+# eg url_for("user", name="Admin!") goes to user func and assigning the name variable the
+# string Admin!
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run()
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from suggestion_model import get_best_suggestion
 from running_model import new_data, run_model
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # Running the model and printing results
-    user_interpolated_prediction, user_salary_band = run_model(new_data, 1)
-    best_suggestion_prediction, biggest_salary_band, category, num_years = get_best_suggestion()
-    #print(best_suggestion_prediction, biggest_salary_band, category, num_years)
-    if user_interpolated_prediction < best_suggestion_prediction:
-        print(f"If you had an increase in {category} then you could go into the salary band {biggest_salary_band} and be "
-              f"estimated to earn {best_suggestion_prediction}")
+user_interpolated_prediction, user_salary_band = run_model(new_data, 1)
+best_suggestion_prediction, biggest_salary_band, category, num_years = get_best_suggestion()
+#print(best_suggestion_prediction, biggest_salary_band, category, num_years)
+if user_interpolated_prediction < best_suggestion_prediction:
+    print(f"If you had an increase in {category} then you could go into the salary band {biggest_salary_band} and be "
+          f"estimated to earn {best_suggestion_prediction}")
